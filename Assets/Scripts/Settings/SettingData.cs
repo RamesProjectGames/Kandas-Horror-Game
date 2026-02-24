2c8dbac857a6e40d43119cf655b5e9125eeab0e5
RamesProjectGames <ramesprojectgames@gmail.com>
2026-02-24 16:43:38 +0700

    Added Fmod for Audio Control

--- Assets/Scripts/Settings/SettingData.cs
+++ Assets/Scripts/Settings/SettingData.cs
@@ -30,4 +30,20 @@ public class SettingData
 
 	public enum TextureQualityLevel { Low = 240, Medium = 360, High = 480 }
 	public TextureQualityLevel TextureQuality = TextureQualityLevel.Low;
+
+	// Control Settings
+	public string AudioInputDeviceName = ""; // Name of the selected input device
+	public int AudioOutputDeviceIndex = 0; // Index in available outputs (if supported)
+	public float MicrophoneSensitivity = 100f; // 1.0 = default
+	public float MouseSensitivity = 1.0f; // 1.0 = default
+	public bool SprintToggle = false; // false = hold to sprint, true = toggle
+
+	// Audio Settings
+	public float MusicVolume = 1.0f;
+	public float SoundEffectVolume = 1.0f;
+	public float MobVolume = 1.0f;
+
+	//Language Settings
+	public enum Language { English, Indonesia }
+	public Language GameLanguage = Language.English;
 }
